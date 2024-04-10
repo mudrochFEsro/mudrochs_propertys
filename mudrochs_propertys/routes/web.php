b66eeaddfaf5e7f97d49d699e09bb49ab9c9e70e
Mudroch.FE s.r.o <wanrltv3328@gmail.com>
2024-04-10 10:25:29 +0200

    Login start

--- mudrochs_propertys/routes/web.php
+++ mudrochs_propertys/routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AutoController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', [ListingController::class, 'index']);
 Route::resource('/', ListingController::class);
 Route::resource('/listing', ListingController::class);
+
+Route::get('login', [AuthController::class, 'create'])
+    ->name('login');
+Route::post('login', [AuthController::class, 'store'])
+    ->name('login.store');
+Route::delete('logout', [AuthController::class, 'destroy'])
+    ->name('logout');
 
