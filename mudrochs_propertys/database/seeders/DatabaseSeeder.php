cdc64e1c6e45aa273b4633d5900451a77c2a71a0
Mudroch.FE s.r.o. <wanrltv3328@gmail.com>
2024-04-23 10:32:48 +0200

    update

--- mudrochs_propertys/database/seeders/DatabaseSeeder.php
+++ mudrochs_propertys/database/seeders/DatabaseSeeder.php
@@ -14,12 +14,26 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        \App\Models\User::factory()->create(['name' => 'Test User 1',
+            'email' => 'test1@example.com',
+            'is_admin' => true
         ]);
-        \App\Models\Listing::factory(20)->create(
+        \App\Models\User::factory()->create([
+            'name' => 'Test User 2',
+            'email' => 'test2@example.com',
+        ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User 3',
+            'email' => 'test3@example.com',
+        ]);
+        \App\Models\Listing::factory(5)->create(
             ['by_user_id' => 1]
+        );
+        \App\Models\Listing::factory(5)->create(
+            ['by_user_id' => 2]
+        );
+        \App\Models\Listing::factory(5)->create(
+            ['by_user_id' => 3]
         );
     }
 }
